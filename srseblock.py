a5b52dcf8ad4912dd7116c94084f9dede1891f46
MKFMIKU <mikumkf@gmail.com>
2018-01-21 11:04:27 +0800

    update relu

--- srseblock.py
+++ srseblock.py
@@ -5,18 +5,20 @@ class SrSEBlock(nn.Module):
     def __init__(self, planes, stride=1):
         super(SrSEBlock, self).__init__()
         self.conv1 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+        self.relu = nn.LeakyReLU(0.2, inplace=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.se = SELayer(planes)
 
     def forward(self, x):
         residual = x
 
-        out = self.relu1(self.conv1(x))
+        out = self.relu(self.conv1(x))
         out = self.conv2(out)
 
         out = self.se(out)
 
+
         out += residual
+        out = self.relu1(out)
 
         return out
